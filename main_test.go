624f06ca9b23a12e361b52b497e331c6eef95d6d
garsue <osuga.shunya@mfkessai.co.jp>
2022-06-06 16:57:38 +0900

    Remove unused field

--- main_test.go
+++ main_test.go
@@ -19,7 +19,6 @@ const (
 )
 
 type Transaction struct {
-	ID          int    `json:"id"`
 	UserID      int    `json:"user_id"`
 	Amount      int    `json:"amount"`
 	Description string `json:"description"`
