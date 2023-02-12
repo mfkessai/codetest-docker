a656a906381dbba2de19fbde3fe6a324bd60cc73
kkb <kkb@kkbs-MacBook-Pro.local>
2023-02-12 18:12:12 +0900

    implement connect sql

--- app/main.go
+++ app/main.go
@@ -4,10 +4,20 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+func init() {
+	_, err := sql.Open("mysql", "root@tcp(127.0.0.1)/codetest")
+	if err != nil {
+		log.Println(err)
+	}
+}
 
 func main() {
 	http.HandleFunc("/transactions", func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r)
 		if _, err := io.Copy(w, r.Body); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
